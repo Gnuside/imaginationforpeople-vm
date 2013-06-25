701b0f1df56017aca427953366a0d16e2a809edb
@@@No user configured@@@ <@@@No user configured@@@>
2013-06-26 00:58:48 +0200

    Add basic packages.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,31 +1,45 @@
 
+class i4p::packages {
+  package { 
+  "graphviz":
+    ensure => installed;
+  "libgraphviz-dev":
+    ensure => installed;
+  "fabric":
+    ensure => installed;
+  "postgis":
+    ensure => installed;
+  "libgdal-dev":
+    ensure => installed;
+  "python-virtualenv":
+    ensure => installed;
+  "git":
+    ensure => installed;
+  }
+}
 
-node "vagrant-debian-squeeze.vagrantup.com" {
-    class{ "apt":  always_apt_update => true; }
+node "vagrant-debian-wheezy.vagrantup.com" {
+  class{ "apt":  always_apt_update => true; }
 
-    apt::key { 
-      "B98321F9": 
-        key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc";
-     "473041FA": 
-        key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc";
+  apt::key { 
+  "B98321F9": 
+  key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc";
+"473041FA": 
+key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc";
       "F42584E6": 
-        key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc"
-    }
+      key_source => "http://ftp-master.debian.org/keys/archive-key-6.0.asc"
+  }
 
-    Apt::Key <| |> -> Exec["apt_update"] 
-    Exec["apt_update"] -> Package <| |>
+  Apt::Key <| |> -> Exec["apt_update"] 
+  Exec["apt_update"] -> Package <| |>
 
-    # globally set exec path
-    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+  # globally set exec path
+  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+  include i4p::packages
 
-    package { 
-      "graphviz":
-        ensure => installed;
-      "libgraphviz-dev":
-        ensure => installed;
-      "fabric":
-       ensure => installed
-    }
+  # FIXME: virtualenv --no-site-packages i4p-env
+  # FIXME: install ruby1.9
+  # FIXME: install rbenv
 
 }
